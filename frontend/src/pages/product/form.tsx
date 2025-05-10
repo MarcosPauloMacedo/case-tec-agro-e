627311bfb764a171f5dbee46e25091aa08d5fefd
Marcos Paulo Macedo <102096687+MarcosPauloMacedo@users.noreply.github.com>
2025-05-10 19:22:56 -0300

    adiciona componentes ProductForm, ProductList e ProductView

---
+++ frontend/src/pages/product/form.tsx
@@ -0,0 +1,8 @@
+export function ProductForm() {
+    return (
+        <div>
+            <h1>Product Form</h1>
+            <p>This is the product form page.</p>
+        </div>
+    )
+}
---
+++ frontend/src/pages/product/view.tsx
@@ -0,0 +1,8 @@
+export function ProductView() {
+    return (
+        <div>
+            <h1>Product View</h1>
+            <p>This is the product view page.</p>
+        </div>
+    )
+}
