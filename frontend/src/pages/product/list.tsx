10b44fe957b875ebd17d2cb790b6f299e4cc8c3f
Marcos Paulo Macedo <102096687+MarcosPauloMacedo@users.noreply.github.com>
2025-05-11 10:04:30 -0300

    adiciona estilos de layout à página de lista de produtos

--- frontend/src/pages/product/list.tsx
+++ frontend/src/pages/product/list.tsx
@@ -1,6 +1,6 @@
 export function ProductList() {
     return (
-        <div>
+        <div className="w-full container mx-auto p-4">
             <h1>Products List</h1>
             <p>This is the products list page.</p>
         </div>
