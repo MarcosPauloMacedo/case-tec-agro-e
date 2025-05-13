ad21d0b5126ea05a2607296ae119bc856f3f4fc1
Marcos Paulo Macedo <102096687+MarcosPauloMacedo@users.noreply.github.com>
2025-05-13 10:27:17 -0300

    altera a cor do título no componente TitlePage para text-emerald-900

--- frontend/src/components/utils/title-page/index.tsx
+++ frontend/src/components/utils/title-page/index.tsx
@@ -2,5 +2,5 @@ interface ITitlePageProps {
   title: string;
 }
 export function TitlePage({ title }: ITitlePageProps) {
-  return <h3 className="text-2xl font-bold mb-4 text-black">{title}</h3>;
+  return <h3 className="text-2xl font-bold mb-4 text-emerald-900">{title}</h3>;
 }
