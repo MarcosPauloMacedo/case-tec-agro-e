432ce94d3ba5346b8b88051af8ff26cc7e9df481
Marcos Paulo Macedo <102096687+MarcosPauloMacedo@users.noreply.github.com>
2025-05-13 10:27:27 -0300

    implementa componente LoadingCircle com animação de carregamento

--- frontend/src/components/utils/loading/circle.tsx
+++ frontend/src/components/utils/loading/circle.tsx
@@ -1,5 +1,7 @@
 export function LoadingCircle() {
-    return (
-        <></>
-    )
+  return (
+    <div className="flex items-center justify-center">
+      <div className="animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-blue-500"></div>
+    </div>
+  );
 }
