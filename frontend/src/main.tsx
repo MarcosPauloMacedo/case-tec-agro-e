6c4d99b9d1836415bbb3a62d70492e0a1cf38219
Marcos Paulo Macedo <102096687+MarcosPauloMacedo@users.noreply.github.com>
2025-05-13 10:30:33 -0300

    adiciona o Provider do Redux ao componente principal para gerenciamento de estado

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,18 @@
-import App from "./App.tsx";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import App from "./App.tsx";
+import { Provider } from "react-redux";
+import { store } from "./provider/product/products-store.tsx";
 
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </StrictMode>
 );
